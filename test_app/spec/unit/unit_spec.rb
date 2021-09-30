8e433649fff467b952a2db86f29573ae371b1c0d
David Tang <davidtang332@gmail.com>
2021-09-30 18:10:49 -0500

    Revert "Revert "Finished integration and unit testing -- Successful""
    This reverts commit d2cb4757ed56dcaa0b635e63d136627d25afa7db.
    
    ffffff

--- test_app/spec/unit/unit_spec.rb
+++ test_app/spec/unit/unit_spec.rb
@@ -3,16 +3,31 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'Percy Jackson', author: 'Rick Riordan', price: '15', published_date: '06/12/2005')
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  
+  it 'is valid title with valid attributes' do
+
+    title = Book.new(title: 'Percy Jackson', author: 'Rick Riordan', price: '15', published_date: '06/12/2005') 
+    expect(title).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
-    expect(subject).not_to be_valid
+  
+  it 'is valid author with valid attributes' do
+
+    author = Book.new(title: 'Percy Jackson', author: 'Rick Riordan', price: '15', published_date: '06/12/2005') 
+    expect(author).to be_valid
+  end
+
+
+  it 'is valid date with valid attributes' do
+    published_date = Book.new(title: 'Percy Jackson', author: 'Rick Riordan', price: '15', published_date: '06/12/2005') 
+    expect(published_date).to be_valid
+  end
+
+  it 'is valid price with valid attributes' do
+    price = Book.new(title: 'Percy Jackson', author: 'Rick Riordan', price: '15', published_date: '06/12/2005') 
+    expect(price).to be_valid
   end
 end
-
